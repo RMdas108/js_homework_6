5bd3478049e956fd25ff0c6e48415df9de957def
Pavlo Braiko <132655086+RMdas108@users.noreply.github.com>
2023-08-16 10:06:00 +0300

    Update 1.1 script.js

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
 //Завдання 1. Гра “Вгадай число” (версія 1.0)
-'use strict'
-let numComp = Math.round(Math.random()*100);
-let i = 1;
-console.log(numComp);
-let numUser = parseInt(prompt("Вгадайте загадане мною число від 0 до 100..."));
-if (!isNaN(numUser) || numUser <= 100 || numUser >= 0 ){
-while (numComp !== numUser){
-  i++; //Рахує кількість спроб;
-  if( numComp < numUser){
-    numUser = +prompt("Введене Вами число більше за те, що я загадав. Спробуйте ще...");
+"use strict";
+var userConfirm = true;
+while (userConfirm) {
+  let numComp = Math.round(Math.random() * 100);
+  let i = 1;
+  let numUser = parseInt(
+    prompt("Вгадайте загадане мною число від 0 до 100...")
+  );
+
+  while (numUser !== null) {
+    console.log(`Компьютер: ${numComp}, Користувач: ${numUser}`); //Введені значення
+    if (!isNaN(numUser) && numUser >= 0 && numUser <= 100) {
+      while (numComp !== numUser) {
+        i++;
+        if (numComp < numUser) {
+          numUser = +prompt(
+            "Введене Вами число БІЛЬШЕ за те, що я загадав. Спробуйте ще..."
+          );
+        } else if (numComp > numUser) {
+          numUser = +prompt(
+            "Введене Вами число МЕНШЕ за те, що я загадав. Спробуйте ще..."
+          );
+        }
+      }
+      userConfirm = confirm(
+        `Вітаю! Ви вгадали з ${i} спроби. Бажаєте пограти ще?`
+      );
+      break;
+    } else {
+      numUser = parseInt(prompt("Введіть число потрібного діапазону."));
+    }
   }
-  else if (numComp > numUser){
-    numUser = +prompt("Введене Вами число менше за те, що я загадав. Спробуйте ще...");}
-  }
-  alert(`Вітаю! Ви вгадали з ${i} спроби.`);
 }
-else {numUser =+prompt("Введіть число потрібного діапазону.")}
